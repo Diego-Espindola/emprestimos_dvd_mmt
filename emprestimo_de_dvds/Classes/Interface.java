defc1811c60c7a9f41753bc11101b5141f4623f1
CristianJucoski <cristian.jucoski@gmail.com>
2022-04-12 23:02:52 -0300

    Correção na Classe

--- emprestimo_de_dvds/Classes/Interface.java
+++ emprestimo_de_dvds/Classes/Interface.java
@@ -1,15 +1,27 @@
 public class Interface {
 
-    public void imprime(String);
+    public void imprime(String a){
+
+    }
     
-    public void imprimeMenu();
+    public void imprimeMenu(){
 
-    public DVD leDVD();
+    }
 
-    public Amigo leAmigo();
+    public DVD leDVD(){
 
-    public void leEmprestimo();
+    }
 
-    public void alertaFaixaEtaria(Amigo,DVD);
+    public Amigo leAmigo(){
+
+    }
+
+    public void leEmprestimo(){
+
+    }
+
+    public void alertaFaixaEtaria(Amigo a,DVD b){
+        
+    }
 
 }
