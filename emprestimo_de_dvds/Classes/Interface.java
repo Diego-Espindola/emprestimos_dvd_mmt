f217b4a6813c7bbeafee031955fa2dcc4a26ef4c
CristianJucoski <cristian.jucoski@gmail.com>
2022-04-12 23:00:16 -0300

    Classe Interface criada

---
+++ emprestimo_de_dvds/Classes/Interface.java
@@ -0,0 +1,15 @@
+public class Interface {
+
+    public void imprime(String);
+    
+    public void imprimeMenu();
+
+    public DVD leDVD();
+
+    public Amigo leAmigo();
+
+    public void leEmprestimo();
+
+    public void alertaFaixaEtaria(Amigo,DVD);
+
+}
