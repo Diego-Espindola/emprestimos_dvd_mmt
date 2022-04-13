fdf8085780f9622a72d75189a5461e2b7b685772
Diego Espindola <diegoespindola9404@gmail.com>
2022-04-12 21:50:26 -0300

    main

--- emprestimo_de_dvds/Main.java
+++ emprestimo_de_dvds/Main.java
@@ -1,7 +1,16 @@
 public class Main{
     public static void main(String[] args){
 
+        public void cadastraAmigos(){
 
+        }
+        public void cadastraDVD(){
+
+        }
+
+        public void regristraEmprestimos(){
+            
+        }
 
 
     }
